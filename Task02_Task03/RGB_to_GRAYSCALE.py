6b2fab464546d51311054c22d9e727d4d5a909d5
Zainab Kashif <zkashif.bscs21seecs@student.nust.edu.pk>
2023-09-25 18:10:06 +0500

    RGB to Binary(TASK 3)

--- Task02_Task03/RGB_to_GRAYSCALE.py
+++ Task02_Task03/RGB_to_GRAYSCALE.py
@@ -5,4 +5,4 @@ cv2.waitKey(0) # 0 means wait for any keypress
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # BGR to GRAY
 cv2.imshow('Grayscale', gray_image) # Displaying the grayscale image
 cv2.waitKey(0)
-cv2.destroyAllWindows() # Destroy all windows now
+cv2.destroyAllWindows() # Destroy all windows 
